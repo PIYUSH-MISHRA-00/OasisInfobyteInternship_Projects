e653fd7238f3365ac7c733a3f5a280115c50a9fc
PIYUSH-MISHRA-00 <PIYUSH-MISHRA-00>
2023-09-07 02:54:40 +0530

    corrections

--- To Do App/app/src/main/java/com/example/todoapp/LoginActivity.java
+++ To Do App/app/src/main/java/com/example/todoapp/LoginActivity.java
@@ -1,54 +1,69 @@
 package com.example.todoapp;
 
-import static com.example.todoapp.databinding.ActivityLoginBinding.inflate;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
-
-import com.example.todoapp.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
-    ActivityLoginBinding binding;
+    // Declare UI elements
+    EditText emailEditText, passwordEditText;
+    Button loginButton;
+    TextView signupRedirectText;
     DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityLoginBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_login);
 
+        // Initialize UI elements
+        emailEditText = findViewById(R.id.login_email);
+        passwordEditText = findViewById(R.id.login_password);
+        loginButton = findViewById(R.id.loginButton);
+        signupRedirectText = findViewById(R.id.signupRedirectText);
+
+        // Initialize database helper
         databaseHelper = new DatabaseHelper(this);
 
-        binding.logingButton.setOnClickListener(new View.OnClickListener() {
+        // Set click listener for login button
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
-                String email = binding.loginEmail.getText().toString();
-                String password = binding.loginPassword.getText().toString();
+            public void onClick(View view) {
+                // Retrieve email and password from EditText fields
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
 
-                if (email.equals("") || password.equals(""))
-                    Toast.makeText(LoginActivity.this, "All fields are mandotory", Toast.LENGTH_SHORT).show();
-                else{
-                    Boolean checkCredentials = databaseHelper.checkEmailPassword(email, password);
+                // Check if email and password are not empty
+                if (email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "All fields are mandatory", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Check if credentials are valid using your DatabaseHelper
+                    boolean checkCredentials = databaseHelper.checkEmailPassword(email, password);
 
-                    if (checkCredentials == true){
+                    if (checkCredentials) {
+                        // If credentials are valid, show a toast message and navigate to MainActivity
                         Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
                         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                         startActivity(intent);
                     } else {
+                        // If credentials are invalid, show a toast message
                         Toast.makeText(LoginActivity.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
                     }
                 }
-        }
+            }
         });
 
-        binding.signupRedirectText.setOnClickListener(new View.OnClickListener() {
+        // Set click listener for signup redirection text
+        signupRedirectText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Navigate to SignupActivity when the text is clicked
                 Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
                 startActivity(intent);
             }
